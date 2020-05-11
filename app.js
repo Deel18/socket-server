aaaf8d61b4812be74b2e64efe8f49506a1a103b5
deel18 <deel18.github@gmail.com>
2020-05-11 15:11:12 +0100

    Added mongodb for chat history

--- app.js
+++ app.js
@@ -5,30 +5,40 @@ const cors = require('cors');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+//Chat db
+const chatDb = require("./data/db.js");
+const url = 'mongodb://localhost:27017/chat';
+const port = 9002;
 
-io.origins(['https://deel-ramverk.me:443']);
+
+io.origins(['https://deel-ramverk.me:443', 'localhost:3000']);
 
 io.on('connection', socket => {
     console.info("User connected");
-    console.info(socket.id)
-    let username = "Anonymous" + Math.floor((Math.random() * 100) + 1);
-    let user = {
-        time: new Date().toLocaleTimeString(),
-        username: username,
-        msg: username + " has joined the chat!"
-    }
 
-    io.emit("recChatMsg", user);
+    chatDb.getColl(url, 'messages')
+    .then(res => io.emit('allMsgs', res))
+    .catch(err => console.log(err))
+
+    //io.emit("recChatMsg", user);
+
+    socket.on("userJoined", (msg) => {
+        chatDb.insertMsg(url, "messages", msg)
+        io.emit("recChatMsg", msg)
+        console.log(msg)
+    })
 
     io.on("disconnect", socket => {
         console.info("Disconnected");
     })
 
     socket.on("sendMsg", (msg) => {
+        chatDb.insertMsg(url, "messages", msg)
         io.emit("recChatMsg", msg)
         console.info(msg)
     })
 
 });
 
-server.listen(9002);
+server.listen(port);
+console.log("Server is listening on port: " + port)
